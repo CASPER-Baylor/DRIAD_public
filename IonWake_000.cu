3c16be1ac4d6397a274b30e1069240fc63736ed6
lswintmatthews <lorin_matthews@baylor.edu>
2018-08-30 14:27:39 -0500

    Commented out "outside ion" forces acting on dust.

--- IonWake_000.cu
+++ IonWake_000.cu
@@ -2073,25 +2073,27 @@ int main(int argc, char* argv[])
 				//accDust[j].z -= q_div_m * E_FIELD  *
 			//(4.0*floor(FREQ*dust_time)-2.0*floor(2.0*FREQ*dust_time)+1.);
 
-				// forces from ions outside simulation region
-				rad = sqrt(posDust[j].x * posDust[j].x +
-							posDust[j].y * posDust[j].y);
-				zsq = posDust[j].z * posDust[j].z;
-
-				radAcc = P10X + P12X * zsq + P14X * zsq * zsq;
-				vertAcc = P01Z * posDust[j].z +
-						  P21Z * rad * rad * posDust[j].z +
-						  P03Z * posDust[j].z * zsq +
-						  P23Z * rad * rad * posDust[j].z * zsq +
-						  P05Z * posDust[j].z * zsq * zsq;
-				accDust[j].x += posDust[j].x * radAcc * q_div_m;
-				accDust[j].y += posDust[j].y * radAcc * q_div_m;
-				accDust[j].z += vertAcc * q_div_m;
-
-				debugSpecificFile << "outside ion acceleration  ";
-				debugSpecificFile << posDust[j].x*radAcc*q_div_m;
-				debugSpecificFile << ", " << posDust[j].y*radAcc*q_div_m;
-				debugSpecificFile << ", " << vertAcc*q_div_m << "\n";
+			 /***************************************
+		*		// forces from ions outside simulation region
+		*		rad = sqrt(posDust[j].x * posDust[j].x +
+		*					posDust[j].y * posDust[j].y);
+		*		zsq = posDust[j].z * posDust[j].z;
+		*
+		*		radAcc = P10X + P12X * zsq + P14X * zsq * zsq;
+		*		vertAcc = P01Z * posDust[j].z +
+		*			  P21Z * rad * rad * posDust[j].z +
+		*			  P03Z * posDust[j].z * zsq +
+		*			  P23Z * rad * rad * posDust[j].z * zsq +
+		*			  P05Z * posDust[j].z * zsq * zsq;
+		*		accDust[j].x += posDust[j].x * radAcc * q_div_m;
+		*		accDust[j].y += posDust[j].y * radAcc * q_div_m;
+		*		accDust[j].z += vertAcc * q_div_m;
+		*
+		*		debugSpecificFile << "outside ion acceleration  ";
+		*		debugSpecificFile << posDust[j].x*radAcc*q_div_m;
+		*		debugSpecificFile << ", " << posDust[j].y*radAcc*q_div_m;
+		*		debugSpecificFile << ", " << vertAcc*q_div_m << "\n";
+			**********************************************/
 
 				// drag force
 				accDust[j].x -= BETA*velDust[j].x;
