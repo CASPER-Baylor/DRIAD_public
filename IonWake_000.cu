96b457b70a41ea749b78d7c2ab2eaef4ef71f0ac
lswintmatthews <lorin_matthews@baylor.edu>
2019-06-10 10:13:31 -0500

    Changed dust-dust interaction from shielded to Coulomb
    The shielding is provided by the ions, not electrons, so no need to
    add additional shielding.

--- IonWake_000.cu
+++ IonWake_000.cu
@@ -1994,11 +1994,11 @@ int main(int argc, char* argv[])
 					dist = sqrt(distSquared);
         
 					//calculate a scalar intermediate
-					linForce=DUST_DUST_ACC_MULT*(simCharge[j]) 
-						* (simCharge[g]) / (dist*dist*dist)
-						* (1.0+dist/DEBYE) * exp(-dist/DEBYE);
-					//linForce=DUST_DUST_ACC_MULT*(chargeDust[j]) 
-					//	* (chargeDust[g]) / (dist*dist*dist);
+					//linForce=DUST_DUST_ACC_MULT*(simCharge[j]) 
+					//	* (simCharge[g]) / (dist*dist*dist)
+					//	* (1.0+dist/DEBYE) * exp(-dist/DEBYE);
+					linForce=DUST_DUST_ACC_MULT*(chargeDust[j]) 
+						* (chargeDust[g]) / (dist*dist*dist);
         
 					// add the acceleration to the current dust grain
 					accDust[j].x += linForce * distdd.x;
