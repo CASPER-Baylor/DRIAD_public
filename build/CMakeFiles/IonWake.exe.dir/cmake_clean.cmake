f941e113aea308217e10a8476db4047bc473c1be
BennyRod <benny.rodriguez1@baylor.edu>
2023-09-15 11:23:44 -0500

    Added underscore in opening process

---
+++ build/CMakeFiles/IonWake.exe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/data/saenzb/DRIAD/Version_Vieja/IonWake/bin/IonWake.exe"
+  "/data/saenzb/DRIAD/Version_Vieja/IonWake/bin/IonWake.exe.pdb"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_000.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_100_integrate.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_101_bounds.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_102_ionAcc.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_103_dustAcc.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_104_roadBlock.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake.exe_generated_IonWake_105_ionColl.cu.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake_106_Utilities.cpp.o"
+  "CMakeFiles/IonWake.exe.dir/IonWake_106_Utilities.cpp.o.d"
+  "CMakeFiles/IonWake.exe.dir/cmake_device_link.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/IonWake.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
