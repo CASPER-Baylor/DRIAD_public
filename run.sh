4568d7c9d8596fe7b72a5fbbb7ed538215a40bbf
BennyRod <benny.rodriguez1@baylor.edu>
2023-11-10 14:41:56 -0600

    Add SWINT GPU

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ rm $1_output/* 2> /dev/null
 echo "cd $run_dir" > qsub_submit_script.sh
 echo "./qsub_run_script.sh $1" >> qsub_submit_script.sh
 
-qsub -N $1 -q gpu -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh 
+# Run in KODIAK
+#qsub -N $1 -q gpu -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh 
+
+# Run in SWINT
+qsub -N $1 -q swint -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh
