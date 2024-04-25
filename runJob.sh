dbf1e546d12a1583cedfbcdd9944a76fff411517
BennyRod <benny.rodriguez1@baylor.edu>
2024-04-25 14:08:53 -0500

    Fix name in running scripts

--- runJob.sh
+++ runJob.sh
@@ -7,7 +7,7 @@ echo "cd $run_dir" > qsub_submit_script.sh
 echo "./qsub_run_script.sh $1" >> qsub_submit_script.sh
 
 # Run in KODIAK
-#qsub -N $1 -q gpu -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh > $1_output/qsub_jobID.txt
+qsub -N $1 -q gpu -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh > $1_output/qsub_jobID.txt
 
 # Run in SWINT
-qsub -N $1 -q swint -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh > $1_output/$1_jobID.txt 
+#qsub -N $1 -q swint -l nodes=1:ppn=18 -o $1_output/qsub_stdout.txt -e $1_output/qsub_stderr.txt qsub_submit_script.sh > $1_output/$1_jobID.txt 
--- runLocal.sh
+++ runLocal.sh
@@ -7,4 +7,4 @@ echo "cd $run_dir" > qsub_submit_script.sh
 echo "./qsub_run_script.sh $1" >> qsub_submit_script.sh
 
 # Run in local
-./local_submit_script.sh & > $1_output/qsub_stdout.txt 2> $1_output/qsub_stderr.txt
+./local_run_script.sh $1 & > $1_output/qsub_stdout.txt 2> $1_output/qsub_stderr.txt
