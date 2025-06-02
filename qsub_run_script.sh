429dd1e97dda3fbbf4ac373159c8c38e3424b444
BennyRod <benny.rodriguez1@baylor.edu>
2025-06-02 10:55:00 -0500

    it was added command lines to preofile the code

--- qsub_run_script.sh
+++ qsub_run_script.sh
@@ -1,12 +1,22 @@
-module load cuda10.2/toolkit 
+#module load cuda/12.3              
+#module load cuda12.3/nsight/12.3.2
+#ncu --version
+#nsys --version
 
 # Select one of the GPU configurations(0,1,`idlegpu`)
-#export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=0
 
 echo "Start Time: `date`"
 #echo "Node: `uname -n`"
 #echo "CUDA Device: $CUDA_VISIBLE_DEVICES"
 
 ./IonWake.exe $1_input/ $1_output/ $1
+
+# Profile with ncu
+#ncu --set full --target-processes all --export $1_output/$1_profile.ncu-rep ./IonWake.exe $1_input/ $1_output/ $1 & echo $! > $1_output/$1_jobID.txt
+#ncu --set full -k calcIonAccels_102 -c 1 --export $1_output/$1_profile.ncu-rep ./IonWake.exe $1_input/ $1_output/ $1
+
+# Profile with nsys
+#nsys profile --stats=true --force-overwrite=true --output=$1_output/$1_profile.nsys-rep ./IonWake.exe $1_input/ $1_output/ $1 & echo $! > $1_output/$1_jobID.txt
 
 echo "End Time: `date`"
--- runLocal.sh
+++ runLocal.sh
@@ -6,8 +6,14 @@ rm $1_output/* 2> /dev/null
 echo "cd $run_dir" > qsub_submit_script.sh
 echo "./qsub_run_script.sh $1" >> qsub_submit_script.sh
 
-# Run in local
-./local_run_script.sh $1 > $1_output/qsub_stdout.txt 2> $1_output/qsub_stderr.txt
+./IonWake.exe $1_input/ $1_output/ $1  echo $! > $1_output/$1_jobID.txt
 
-# Run in local in background
-#./local_run_script.sh $1 & > $1_output/qsub_stdout.txt 2> $1_output/qsub_stderr.txt
+#./IonWake.exe $1_input/ $1_output/ $1 & echo $! > $1_output/$1_jobID.txt) > $1_output/qsub_stdout.txt 2> $1_output/qsub_stderr.txt
+
+
+# Profile with ncu
+#ncu --set full --target-processes all --export $1_output/$1_profile.ncu-rep ./IonWake.exe $1_input/ $1_output/ $1 & echo $! > $1_output/$1_jobID.txt
+#ncu --set full -k calcIonAccels_102 -c 1 --export $1_output/$1_profile.ncu-rep ./IonWake.exe $1_input/ $1_output/ $1
+
+# Profile with nsys
+#nsys profile --stats=true --force-overwrite=true --output=$1_output/$1_profile.nsys-rep ./IonWake.exe $1_input/ $1_output/ $1 & echo $! > $1_output/$1_jobID.txt
